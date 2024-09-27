63753d1eb00361c3bf509821e7e0ba90a5d6f5d0
kdoan3 <kbdoan103@student.hbuhsd.edu>
2024-09-27 10:44:35 -0700

    fdfdsa

--- Classwork.java
+++ Classwork.java
@@ -6,5 +6,23 @@ public class Classwork
   }
 
   // write solutions to problems below
+public static String firstLastN(String word, int n)
+{
+  // word = dictionary
+  // n = 3
+  // dicary
+  String output = new String();
+
+// Getting the first n letters
+  String firstN = word.substring(0,n); // first n letters; substring from 0 to n-1
+
+// Getting the last n letters
+  int indexOfNToLast = word.length() - n; // index of the nth to last letter
+  String lastN = word.substring(indeOfNToLast); // last n letters, starting at nth to last letter
+
+  output = firstN + lastN;
+  return output;
+  
+}
 
 }
