c017fc8c5f6854d8716bda7f8fcf42e23fd196e1
kdoan3 <kbdoan103@student.hbuhsd.edu>
2024-09-27 10:50:52 -0700

    fdfdsf

--- Classwork.java
+++ Classwork.java
@@ -22,6 +22,24 @@ public static String firstLastN(String word, int n)
 
   output = firstN + lastN;
   return output;
+
+   
+public static String delEnd(String word, int n)
+{
+  // word = library
+  // n = 5
+  
+  String output = new String();
+
+// Getting the first n letters
+  String firstN = word.substring(0,n); // first n letters; substring from 0 to n-1
+
+// Getting the last n letters
+  int indexOfNToLast = word.length() - n; // index of the nth to last letter
+  String lastN = word.substring(indeOfNToLast); // last n letters, starting at nth to last letter
+
+  output = firstN + lastN;
+  return output;
   
 }
 
